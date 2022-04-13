34bd6f967f0c0e134f61047f46027879826bf69d
santhosh27jd@gmail.com <santhosh27jd@gmail.com>
2022-04-13 09:49:24 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var bool = false;
 var counter=0;
+document.getElementById("scoreSpan").innerHTML = 0;
 function jump(){
+    if(!bool){
+        alert("Please Start the Game");
+        return;
+    }
     if(character.classList == "animate"){return}
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
+}
+function start(){
+    block.style.animation = "block 1s infinite linear";
+    bool=true;
+}
+function reset(){
+    document.getElementById("scoreSpan").innerHTML = 0;
+    block.style.animation = "none";
+    bool = false;
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -15,9 +30,11 @@ var checkDead = setInterval(function() {
         block.style.animation = "none";
         alert("The Game Is Over. SCORE: "+Math.floor(counter/100));
         counter=0;
-        block.style.animation = "block 1s infinite linear";
+        reset();
     }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        if(bool){
+            counter++;
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        }
     }
 }, 10);
